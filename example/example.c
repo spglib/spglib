80db237d0f026584f8edb82e36387529373b9818
Atsushi Togo <atz.togo@gmail.com>
2014-02-07 15:49:53 +0900

    spg_get_spacegroup_type: Space group data direct access

--- example/example.c
+++ example/example.c
@@ -16,6 +16,11 @@ static void test_spg_get_stabilized_reciprocal_mesh(void);
 static void test_spg_get_tetrahedra_relative_grid_address(void);
 static void test_spg_relocate_BZ_grid_address(void);
 static void test_spg_triplets_reciprocal_mesh_at_q(void);
+static void show_spg_dataset(double lattice[3][3],
+			     const double origin_shift[3],
+			     double position[][3],
+			     const int num_atom,
+			     const int types[]);
 static int grid_address_to_index(int g[3], int mesh[3]);
 static void mat_copy_matrix_d3(double a[3][3], double b[3][3]);
 static double mat_get_determinant_d3(double a[3][3]);
@@ -299,7 +304,7 @@ static void test_spg_get_symmetry_with_collinear_spin(void) {
 
 static void test_spg_get_dataset(void)
 {
-  SpglibDataset *dataset;
+  printf("*** Example of spg_get_dataset (Rutile two unit cells) ***:\n");
   double lattice[3][3] = {{4,0,0},{0,4,0},{0,0,3}};
   double origin_shift[3] = { 0.1, 0.1, 0 };
   double position[][3] =
@@ -319,58 +324,24 @@ static void test_spg_get_dataset(void)
     };
   int types[] = {1,1,2,2,2,2,1,1,2,2,2,2};
   int num_atom = 12;
-  int i, j, size;
-  const char *wl = "abcdefghijklmnopqrstuvwxyz";
 
-  for ( i = 0; i < num_atom; i++ ) {
-    for ( j = 0; j < 3; j++ ) {
-      position[i][j] += origin_shift[j];
-    }
-  }
+  show_spg_dataset(lattice, origin_shift, position, num_atom, types);
 
-  printf("*** Example of spg_get_dataset (Rutile two unit cells) ***:\n");
-  dataset = spg_get_dataset(lattice,
-			    position,
-			    types,
-			    num_atom,
-			    1e-5);
-  
-  printf("International: %s (%d)\n", dataset->international_symbol, dataset->spacegroup_number );
-  printf("Hall symbol:   %s\n", dataset->hall_symbol );
-  printf("Transformation matrix:\n");
-  for ( i = 0; i < 3; i++ ) {
-    printf("%f %f %f\n",
-	   dataset->transformation_matrix[i][0],
-	   dataset->transformation_matrix[i][1],
-	   dataset->transformation_matrix[i][2]);
-  }
-  printf("Wyckoff letters:\n");
-  for ( i = 0; i < dataset->n_atoms; i++ ) {
-    printf("%c ", wl[dataset->wyckoffs[i]]);
-  }
-  printf("\n");
-  printf("Equivalent atoms:\n");
-  for (i = 0; i < dataset->n_atoms; i++) {
-    printf("%d ", dataset->equivalent_atoms[i]);
-  }
-  printf("\n");
-  
-  for (i = 0; i < dataset->n_operations; i++) {
-    printf("--- %d ---\n", i + 1);
-    for (j = 0; j < 3; j++) {
-      printf("%2d %2d %2d\n",
-	     dataset->rotations[i][j][0],
-	     dataset->rotations[i][j][1],
-	     dataset->rotations[i][j][2]);
-    }
-    printf("%f %f %f\n",
-	   dataset->translations[i][0],
-	   dataset->translations[i][1],
-	   dataset->translations[i][2]);
-  }
+  double lattice_2[3][3] = {{3.7332982433264039, -1.8666491216632011, 0},
+			    {0, 3.2331311186244847, 0},
+			    {0, 0, 6.0979971306362799}};
+  double origin_shift_2[3] = { 0.1, 0.1, 0 };
+  double position_2[][3] =
+    {
+      {0, 0, 0},
+      {1.0 / 3, 2.0 / 3, 0.4126},
+      {1.0 / 3, 2.0 / 3, 0.776},
+      {2.0 / 3, 1.0 / 3, 0.2542},
+    };
+  int types_2[] = {1,2,3,3};
+  int num_atom_2 = 4;
 
-  spg_free_dataset(dataset);
-
+  show_spg_dataset(lattice_2, origin_shift_2, position_2, num_atom_2, types_2);
 }
 
 static void test_spg_get_ir_reciprocal_mesh(void)
@@ -742,6 +713,75 @@ static void test_spg_get_tetrahedra_relative_grid_address(void)
   fclose(fp);
 }
 
+
+static void show_spg_dataset(double lattice[3][3],
+			     const double origin_shift[3],
+			     double position[][3],
+			     const int num_atom,
+			     const int types[])
+{
+  SpglibDataset *dataset;
+  char ptsymbol[6];
+  int pt_trans_mat[3][3];
+
+  int i, j, size;
+  const char *wl = "abcdefghijklmnopqrstuvwxyz";
+
+  for ( i = 0; i < num_atom; i++ ) {
+    for ( j = 0; j < 3; j++ ) {
+      position[i][j] += origin_shift[j];
+    }
+  }
+
+  dataset = spg_get_dataset(lattice,
+			    position,
+			    types,
+			    num_atom,
+			    1e-5);
+  
+  printf("International: %s (%d)\n", dataset->international_symbol, dataset->spacegroup_number );
+  printf("Hall symbol:   %s\n", dataset->hall_symbol );
+  spg_get_pointgroup(ptsymbol,
+		     pt_trans_mat,
+		     dataset->rotations,
+		     dataset->n_operations);
+  printf("Point group:   %s\n", ptsymbol);
+  printf("Transformation matrix:\n");
+  for ( i = 0; i < 3; i++ ) {
+    printf("%f %f %f\n",
+	   dataset->transformation_matrix[i][0],
+	   dataset->transformation_matrix[i][1],
+	   dataset->transformation_matrix[i][2]);
+  }
+  printf("Wyckoff letters:\n");
+  for ( i = 0; i < dataset->n_atoms; i++ ) {
+    printf("%c ", wl[dataset->wyckoffs[i]]);
+  }
+  printf("\n");
+  printf("Equivalent atoms:\n");
+  for (i = 0; i < dataset->n_atoms; i++) {
+    printf("%d ", dataset->equivalent_atoms[i]);
+  }
+  printf("\n");
+  
+  for (i = 0; i < dataset->n_operations; i++) {
+    printf("--- %d ---\n", i + 1);
+    for (j = 0; j < 3; j++) {
+      printf("%2d %2d %2d\n",
+	     dataset->rotations[i][j][0],
+	     dataset->rotations[i][j][1],
+	     dataset->rotations[i][j][2]);
+    }
+    printf("%f %f %f\n",
+	   dataset->translations[i][0],
+	   dataset->translations[i][1],
+	   dataset->translations[i][2]);
+  }
+
+  spg_free_dataset(dataset);
+
+}
+
 static int grid_address_to_index(int g[3], int mesh[3])
 {
   int i;
@@ -796,3 +836,4 @@ static int mat_inverse_matrix_d3(double m[3][3],
   mat_copy_matrix_d3(m, c);
   return 1;
 }
+
