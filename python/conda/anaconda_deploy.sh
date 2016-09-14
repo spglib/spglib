ceb9cd65ba99b9f40cc81fc35472d68a374a5ea7
Pawel T. Jochym <jochym@wolf.ifj.edu.pl>
2016-09-14 18:46:46 +0200

    Put * into version string for upload.

--- python/conda/anaconda_deploy.sh
+++ python/conda/anaconda_deploy.sh
@@ -6,7 +6,7 @@ export GIT_BRANCH=$3
 
 conda install conda-build anaconda-client --yes
 conda build conda --no-anaconda-upload
-TRG=`conda build conda --output`
+TRG=`conda build conda --output |sed -e 's/--/-*-/'`
 echo "Uploading: $TRG"
 anaconda --token $TKN upload --label $LBL $TRG
 
