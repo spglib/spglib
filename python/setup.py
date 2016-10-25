f1397cfa8b3a79647e724348f40d0e2463980729
Atsushi Togo <atz.togo@gmail.com>
2016-10-25 10:46:10 +0900

    Fix setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 try:
     from setuptools import setup, Extension
@@ -17,13 +18,12 @@ except ImportError:
     from distutils.core import setup, Extension
     use_setuptools = False
     print("distutils is used.")
-
-try:
-    from numpy.distutils.misc_util import get_numpy_include_dirs
-except ImportError:
-    print("numpy.distutils.misc_util cannot be imported. Please install "
-          "numpy first before installing spglib...")
-    sys.exit(1)
+    try:
+        from numpy.distutils.misc_util import get_numpy_include_dirs
+    except ImportError:
+        print("numpy.distutils.misc_util cannot be imported. Please install "
+              "numpy first before installing spglib...")
+        sys.exit(1)
 
 # Workaround Python issue 21121
 import sysconfig
