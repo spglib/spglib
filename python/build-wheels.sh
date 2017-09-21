37e95a1122e0316316afaf5503f402f5d55ab85b
Atsushi Togo <atz.togo@gmail.com>
2017-09-21 18:41:44 +0900

    .travis.yml test

---
+++ python/build-wheels.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e -x
+
+# Install a system package required by our library
+yum install -y numpy
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    # "${PYBIN}/pip" install numpy
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+# for whl in wheelhouse/*.whl; do
+#     auditwheel repair "$whl" -w /io/wheelhouse/
+# done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
+done
