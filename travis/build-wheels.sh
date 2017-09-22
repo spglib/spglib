d4499ba07ef5bb5b10834ec325b84bc7806b1ff7
Atsushi Togo <atz.togo@gmail.com>
2017-09-22 18:04:47 +0900

    .travis.yml test

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -11,9 +11,9 @@ for PYBIN in /opt/python/*/bin; do
 done
 
 # Bundle external shared libraries into the wheels
-# for whl in wheelhouse/*.whl; do
-#     auditwheel repair "$whl" -w /io/wheelhouse/
-# done
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
