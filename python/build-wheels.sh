918126968f07b0043f5a168c040c48ccc915e006
Atsushi Togo <atz.togo@gmail.com>
2017-09-22 18:57:01 +0900

    .travis.yml test

--- python/build-wheels.sh
+++ python/build-wheels.sh
@@ -3,7 +3,9 @@ set -e -x
 
 # Install a system package required by our library
 yum install -y numpy
+
 ls /opt/python
+ln -s ../src .
 
 # Compile wheels
 for PYBIN in /opt/python/cp27*/bin; do
@@ -11,6 +13,10 @@ for PYBIN in /opt/python/cp27*/bin; do
 done
 
 for PYBIN in /opt/python/cp35*/bin; do
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+for PYBIN in /opt/python/cp36*/bin; do
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
