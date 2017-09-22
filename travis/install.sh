906339ac9e955b52c09187c763a41eb0c89490fb
Atsushi Togo <atz.togo@gmail.com>
2017-09-22 20:43:09 +0900

    .travis.yml test

--- travis/install.sh
+++ travis/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e -x
 
-conda install --yes python="$TRAVIS_PYTHON_VERSION" pip numpy
 aclocal
 autoheader
 libtoolize
@@ -11,11 +10,6 @@ autoconf
 ./configure
 make check # test in C
 cd python
-pwd
-git branch -v
-git describe --tags --dirty
-./get_nanoversion.sh
-cat __nanoversion__.txt
 ln -s ../src .
 python setup.py install 
 cd ..
