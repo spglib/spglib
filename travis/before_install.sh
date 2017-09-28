b1091db806aa1e6715945e64e65f5949a434c936
Atsushi Togo <atz.togo@gmail.com>
2017-09-28 17:19:30 +0900

    Test travis for conda older version

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -2,9 +2,11 @@
 set -e -x
 
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-  wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
+    # wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda2-4.3.21-Linux-x86_64.sh -O miniconda.sh;
 else
-  wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+    # wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda3-4.3.21-Linux-x86_64.sh -O miniconda.sh;
 fi
 echo $TRAVIS_PYTHON_VERSION
 bash miniconda.sh -b -p $HOME/miniconda
