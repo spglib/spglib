9876529fbe26e0a2610c62847f96965b81176e95
Atsushi Togo <atz.togo@gmail.com>
2017-10-02 12:33:35 +0900

    .travis.yml update for conda

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -2,14 +2,12 @@
 set -e -x
 
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-    # wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
-    wget https://repo.continuum.io/miniconda/Miniconda2-4.3.21-Linux-x86_64.sh -O miniconda.sh;
+    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
 else
-    # wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-    wget https://repo.continuum.io/miniconda/Miniconda3-4.3.21-Linux-x86_64.sh -O miniconda.sh;
+    wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 fi
 echo $TRAVIS_PYTHON_VERSION
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
-# conda update --yes conda
+conda update --yes conda
 conda install --no-update-deps --yes python="$TRAVIS_PYTHON_VERSION" pip numpy
