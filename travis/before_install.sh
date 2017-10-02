411f4dfaf91fb3c064d2f0deb6f1e6a617a7aa72
Atsushi Togo <atz.togo@gmail.com>
2017-10-02 12:53:20 +0900

    .travis.yml update for conda python3.5

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e -x
 
-if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
-else
+# UnsatisfiableError: The following specifications were found to be in conflict:
+#   - pyopenssl 17.0.0 py36_0 -> python 3.6*
+#   - python 3.5*
+if [[ "$TRAVIS_PYTHON_VERSION" == "3.6" ]]; then
     wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+else
+    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
 fi
 echo $TRAVIS_PYTHON_VERSION
 bash miniconda.sh -b -p $HOME/miniconda
