4f2979dabe7edbff5183d5bd00d603e4e494cc09
Atsushi Togo <atz.togo@gmail.com>
2020-05-03 21:20:04 +0900

    Update packaging settings

--- python/get_nanoversion.sh
+++ python/get_nanoversion.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-br=`git branch |tail -n1`
+br=`git rev-parse HEAD`
+echo $br
 read o u f <<< `git remote -v |grep origin |grep fetch`
 echo "Repo: $o $u $br"
-
-# This doesn't work well for rc because it is supposed tag is not pushed yet.
 git describe --tags --dirty
-
 TD=`mktemp -d`
 WD=`pwd`
-
-# full clone is necessary to count number of commits after tag.
-echo "Run 'git clone $u $TD'"
-git clone --depth 1 $u $TD
+git branch
+git clone $u $TD
 cd $TD
-echo $u $br
 git checkout $br
-git describe --tags --dirty | sed -e 's/\([.0-9]*\)-\(.*\)-g.*/\2/' -e 's/^[vr]//g' >$WD/__nanoversion__.txt
+echo $u $br
+git describe --tags --dirty | sed -e 's/\([.0-9]*\)-\(.*\)-g.*/\2/' -e 's/^[vr]//g' -e 's/rc-//g' > $WD/__nanoversion__.txt
 cd $WD
 rm -rf "$TD"
