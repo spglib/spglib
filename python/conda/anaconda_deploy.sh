7ef0005b0d8b90891925df29b28ec31b6a645405
Atsushi Togo <atz.togo@gmail.com>
2020-07-30 23:19:46 +0900

    Update anaconda_deploy.sh

--- python/conda/anaconda_deploy.sh
+++ python/conda/anaconda_deploy.sh
@@ -11,8 +11,8 @@ echo "-----------------------"
 echo "GIT_BRANCH: $GIT_BRANCH"
 echo "-----------------------"
 cd ..
-conda install conda-build anaconda-client --yes
-conda build conda --no-anaconda-upload
-TRG=`conda build conda --output |sed -e 's/--/-*-/'`
+conda install -c https://conda.anaconda.org/conda-forge conda-build anaconda-client --yes
+conda build conda -c https://conda.anaconda.org/conda-forge --no-anaconda-upload
+TRG=`conda build conda -c https://conda.anaconda.org/conda-forge --output |sed -e 's/--/-*-/'`
 echo "Uploading: $TRG"
 anaconda --token $TKN upload --label $LBL $TRG
