9845744a8a1d2fca9f727e5e1c09b1dad75aa7bf
Atsushi Togo <atz.togo@gmail.com>
2023-02-05 21:35:23 +0900

    Fix test_fortran_spg_refine_cell::test_FCC

--- test/fortran/test_fortran_spg_refine_cell.f90
+++ test/fortran/test_fortran_spg_refine_cell.f90
@@ -4,25 +4,30 @@ program test_fortran_spg_refine_cell
     use test_utils
     implicit none
 
-    write (*, '("test_BCC")')
-    call test_BCC()
+    write (*, '("test_FCC")')
+    call test_FCC()
 contains
-    subroutine test_BCC() bind(C)
+    !> @brief Find standardaized cell of FCC from its primitive cell
+    !> The output structure of spg_refine_cell may contain more number of atoms.
+    !> Therefore, larger space than that required for input structure has to
+    !> be allocated.
+    subroutine test_FCC() bind(C)
         real(c_double) :: lattice(3, 3)
-        real(c_double) :: position(3, 1)
+        real(c_double) :: position(3, 4)
         integer(c_int) :: types(4)
         integer :: num_atom_bravais
         integer(c_int) :: num_atom
         real(c_double) :: symprec
 
-        lattice(:, :) = reshape([0, 2, 2, 2, 0, 2, 2, 2, 0], [3, 3])
-        position(:, :) = reshape([0, 0, 0], [3, 1])
-        types(1) = 0  ! overwritten (maximum 4 times of number of input atoms)
         num_atom = 1
+        lattice(:, :) = reshape([0, 2, 2, 2, 0, 2, 2, 2, 0], [3, 3])
+        ! position and types : 4 times of number of input atoms at maximum
+        position(:, 1) = 0.0
+        types(1) = 0
         symprec = 1e-5
 
         num_atom_bravais = spg_refine_cell(lattice, position, types, num_atom, symprec)
 
         call assert_int(num_atom_bravais, 4)
-    end subroutine test_BCC
+    end subroutine test_FCC
 end program test_fortran_spg_refine_cell
