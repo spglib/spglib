2d0b73cc3b556282e35f9ae51de01268f5d88e5a
Jingheng Fu <fujh18@mails.tsinghua.edu.cn>
2023-08-21 19:06:13 +0800

    refactor(database): standardize Hall symbols

--- database/hall2operations.py
+++ database/hall2operations.py
@@ -333,10 +333,11 @@ class HallSymbol:
         return T
 
     def _change_of_basis_symbol(self, V):
-        if V[0] == '-':
-            return np.array([0, 0, -1])
-        else:
-            return np.array([0, 0, 1])
+        # if V[0] == '-':
+        #     return np.array([0, 0, -1])
+        # else:
+        #     return np.array([0, 0, 1])
+        return np.array([0, 0, int(V[:-1])])
 
 
 def dump_operations(filename):
