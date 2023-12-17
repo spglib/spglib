0952feeb108d0fd192a608659c58cf99dc2f5169
Jingheng Fu <fujh18@mails.tsinghua.edu.cn>
2023-12-17 17:47:07 +0800

    fix(symmetry): fix matrix pattern for layer groups

--- src/symmetry.c
+++ src/symmetry.c
@@ -961,9 +961,9 @@ static PointSymmetry get_lattice_symmetry(const Cell *cell,
                 for (k = 0; k < 26; k++) {
                     /* For layer group, some rotations are not permitted. */
                     if (aperiodic_axis != -1 &&
-                        ((aperiodic_axis == 2 && k != 2 && k != 5) ||
-                         (aperiodic_axis == 0 && i != 0 && i != 3) ||
-                         (aperiodic_axis == 1 && j != 1 && j != 4))) {
+                        ((aperiodic_axis == 2 && ((k != 2 && k != 5) || (i != 0 && i != 1 && i != 3 && i != 4 && i != 8 && i != 11 && i != 14 && i != 17) || (j != 0 && j != 1 && j != 3 && j != 4 && j != 8 && j != 11 && j != 14 && j != 17))) ||
+                         (aperiodic_axis == 0 && ((i != 0 && i != 3) || (j != 1 && j != 2 && j != 4 && j != 5 && j != 6 && j != 9  && j != 12 && j != 15) || (k != 1 && k != 2 && k != 4 && k != 5 && k != 6 && k != 9  && k != 12 && k != 15))) ||
+                         (aperiodic_axis == 1 && ((j != 1 && j != 4) || (i != 0 && i != 2 && i != 3 && i != 5 && i != 7 && i != 10 && i != 13 && i != 16) || (k != 0 && k != 2 && k != 3 && k != 5 && k != 7 && k != 10 && k != 13 && k != 16))))) {
                         continue;
                     }
                     set_axes(axes, i, j, k);
