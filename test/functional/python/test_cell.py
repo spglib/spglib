2ba078ed110b952e1bda065d7796856987b0d3e1
lan496 <kshinohara0508@gmail.com>
2024-02-11 11:07:08 +0900

    Match exception message

--- test/functional/python/test_cell.py
+++ test/functional/python/test_cell.py
@@ -3,6 +3,5 @@ from spglib.spglib import _expand_cell
 
 
 def test_expand_cell():
-    with pytest.raises(TypeError) as e:
-        _expand_cell(None)
-    assert e.value
+    with pytest.raises(TypeError, match="cell has to be a tuple of 3 or 4 elements."):
+        _expand_cell(([], [], [], [], []))
